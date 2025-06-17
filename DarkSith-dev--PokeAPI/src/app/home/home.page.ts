1dced3b1179e35698086df70243577d2fb467607
Diego Alves Rodrigues <51385145+DarkSith-dev@users.noreply.github.com>
2025-06-17 20:31:14 -0300

    feat (home): exebir cards dos pokemons  e icon do menu
    Signed-off-by: Diego Alves Rodrigues <51385145+DarkSith-dev@users.noreply.github.com>

---
+++ DarkSith-dev--PokeAPI/src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [
+    CommonModule,
+    IonicModule
+  ]
+})
+export class HomePage {
+  constructor() {}
+}
